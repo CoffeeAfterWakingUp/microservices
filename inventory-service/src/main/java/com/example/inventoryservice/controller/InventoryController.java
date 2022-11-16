5e03e1b90c2d547c0a320ba6a0c9a7e8f6fcbb5a
Сырбек Олжас Бекенулы <olzhas.syrbek@ffin.kz>
2022-11-16 17:12:52 +0600

    Inital Commit

---
+++ inventory-service/src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,29 @@
+package com.example.inventoryservice.controller;
+
+import com.example.inventoryservice.dto.InventoryResponse;
+import com.example.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        log.info("Request to isInStock with {}", Arrays.toString(skuCode.toArray()));
+        return inventoryService.isInStock(skuCode);
+    }
+
+}
---
+++ product-service/src/main/java/com/example/productservice/model/Product.java
@@ -0,0 +1,33 @@
+package com.example.productservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
